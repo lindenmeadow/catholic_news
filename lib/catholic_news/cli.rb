3b7c4c2ebbf15b0bbc3805036a288681e33a307e
Evan B <konros@yahoo.com>
2019-08-21 13:20:17 -0400

    Update cli.rb

--- lib/catholic_news/cli.rb
+++ lib/catholic_news/cli.rb
@@ -1,47 +1,50 @@
 class CatholicNews::CLI
-	def call
-		list_headlines
-		info
-		selection
+  attr_reader :headline, :teaser, :link
+  def call
+    selection
+  end
+	
+  def list_headlines
+	puts "Welcome to catholicnewsagency.com"
+	puts "Today's Headlines"
+	puts "-----------------"
+	puts ""
+	@story = CatholicNews::News.all_headlines
+	    @story.each.with_index(1) do |h, i|
+	      puts "#{i}. #{h}"
+	      puts "-------------------------"
+	    end 
 	end
 	
-	def list_headlines
-		puts "Today's Headlines"
-		puts "_________________"
-		CatholicNews::News.all.each.with_index(1) do |s, i|
-			puts "#{i}. #{s.headline}"
-		end
-	end
-	
-	def info(story)
-		puts "#{story.headline}"
-		puts ""
-		puts story.teaser
-		puts ""
-		puts "URL: #{story.link}"
-		puts ""
-	end
-	
-	def selection
-		list_headlines
-		input = nil
+  def selection
+	list_headlines
+	input = nil
 		
-		while input != "exit"
-			puts "Options:"
-			puts "Enter the number of the story you would like more information on," 
-			puts "Type headlines to see the list of headlines again,"
-			puts "Or type exit to quit"
-			input = gets.strip.downcase
-				if input == "headlines"
-					list_headlines
-				elsif input.to_i > 0
-					if story = NewsScraper::News.find(input.to_i)
-						info(story)
-					end
-				else 
-					puts "Invalid entry. Please enter headlines, exit, or a number from the list of headlines."
-				end
-		end
-		puts "Thanks for visiting! Check back tomorrow for more news!"
+	while input != "exit"
+		puts "Options:"
+		puts "-Type the number of the headline for more information"
+		puts "-Type headlines to see the list of headlines again"
+		puts "-Or type exit to quit"
+			
+		input = gets.strip.downcase
+			if input == "headlines"
+			     list_headlines
+			elsif input.to_i > 0 && input.to_i <= 20
+			     puts "~~~~~~~~~~~~~~~~~~~~~~~~~"
+			     puts "--> " + CatholicNews::News.find_teaser(input.to_i)
+			     puts ""
+			     puts "Full story: www.catholicnewsagency.com" + CatholicNews::News.find_link(input.to_i)
+			     puts "~~~~~~~~~~~~~~~~~~~~~~~~~"
+			elsif input == "exit"
+			     finish
+			else
+			     puts "Invalid entry."
+			end
 	end
+  end
+	
+  def finish
+       puts "Thanks for visiting! Check back tomorrow for more news!"
+  end
+	
 end
