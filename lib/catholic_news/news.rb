2bec68be73042419ea68e824f413bd7a3ace02cd
Evan B <konros@yahoo.com>
2019-08-26 16:52:54 -0400

    Update news.rb

--- lib/catholic_news/news.rb
+++ lib/catholic_news/news.rb
@@ -10,19 +10,19 @@ class CatholicNews::News
 
 	def self.teasers
 	  doc = Nokogiri::HTML(open("https://www.catholicnewsagency.com/headlines"))
-	  teaser = doc.search("div.noticia_list_body")
+	  teaser = doc.css("div.noticia_list_body")
 	  teaser.collect{|t| t.text.strip}
 	end
 	
 	def self.links
 	  doc = Nokogiri::HTML(open("https://www.catholicnewsagency.com/headlines"))
-	  link = doc.search("div.noticia_list_title h3 b a")
+	  link = doc.css("div.noticia_list_title h3 b a")
 	  link.collect{|l| l.attr("href")}
 	end
 	
 	def self.headlines
 	  doc = Nokogiri::HTML(open("https://www.catholicnewsagency.com/headlines"))
-	  headline ||= doc.search("div.noticia_list_title")
+	  headline ||= doc.css("div.noticia_list_title")
 	  headline.collect{|h| h.text.strip}
 	end
 end
