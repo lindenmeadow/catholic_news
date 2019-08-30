ba56174cabbbe13bcc61589fba6d6a7d84872221
Evan B <konros@yahoo.com>
2019-08-29 21:50:18 -0400

    Update news.rb

--- lib/catholic_news/news.rb
+++ lib/catholic_news/news.rb
@@ -1,29 +1,13 @@
 class CatholicNews::News
 	
-	def self.access_page
-	  Nokogiri::HTML(open("https://www.catholicnewsagency.com/headlines"))
-	end
+	def news_stories
+		@doc = Nokogiri::HTML(open("https://www.catholicnewsagency.com/headlines"))
 
-	def self.find_teaser(id)
-	  self.teasers[id-1]
-	end
-
-	def self.find_link(l)
-	  self.links[l-1]
-	end
-
-	def self.teasers
-	  @teasers = access_page.search("div.noticia_list_body")
-	  @teasers.collect{|t| t.text.strip}
-	end
-
-	def self.links
-	  @links = access_page.search("div.noticia_list_title h3 b a")
-	  @links.collect{|l| l.attr("href")}
-	end
-
-	def self.headlines
-	  @headlines = access_page.search("div.noticia_list_title")
-	  @headlines.collect{|h| h.text.strip}
+		@doc.css("div.noticia_list_contenedor").each do |st|
+			story = CatholicNews::Story.new
+			story.headline = st.search("div.noticia_list_title").text.strip
+			story.teaser = st.search("div.noticia_list_body").text.strip
+			story.link = st.search("div.noticia_list_title h3 b a").attr("href")
+		end
 	end
 end
