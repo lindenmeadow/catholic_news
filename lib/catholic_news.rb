7f686fa6bc59a5a0be9a843aa1d44fe17a4035fa
Evan B <konros@yahoo.com>
2019-08-29 21:46:37 -0400

    Update catholic_news.rb

--- lib/catholic_news.rb
+++ lib/catholic_news.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 require_relative "./catholic_news/version.rb"
 require_relative "./catholic_news/cli.rb"
 require_relative "./catholic_news/news.rb"
+require_relative "./catholic_news/story.rb"
 
 module CatholicNews
   class Error < StandardError; end
