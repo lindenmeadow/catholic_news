4ef0d0c263418b8ebaf13249976c0f318b9f6541
Evan B <konros@yahoo.com>
2019-08-31 15:04:07 -0400

    Update news.rb

--- lib/catholic_news/news.rb
+++ lib/catholic_news/news.rb
@@ -5,9 +5,9 @@ class CatholicNews::News
 
 		@doc.css("div.noticia_list_contenedor").each do |st|
 			story = CatholicNews::Story.new
-			story.headline = st.css("div.noticia_list_title").text.strip
-			story.teaser = st.css("div.noticia_list_body").text.strip
-			story.link = st.css("div.noticia_list_title h3 b a").attr("href")
+			story.headline = st.search("div.noticia_list_title").text.strip
+			story.teaser = st.search("div.noticia_list_body").text.strip
+			story.link = st.search("div.noticia_list_title h3 b a").attr("href")
 		end
 	end
 end
